6dbf814855b71c7ad3aa8a6b5f1748ee845f0798
German Fondevila <germanfondevila@gmail.com>
2016-10-05 16:40:12 -0400

    final push

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length of rectangle: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter width of rectangle: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int x = Area(a , b);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        //calculate the area of a rectangle
+        static int Area(int x, int y)
+        {
+            return (x * y);
+        }
+    }
+}
