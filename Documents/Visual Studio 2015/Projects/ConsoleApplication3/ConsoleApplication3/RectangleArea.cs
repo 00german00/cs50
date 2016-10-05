b3ec4973c8cd7e808fbfd2fd2b68b056e36cf5b7
German Fondevila <germanfondevila@gmail.com>
2016-10-05 14:04:39 -0400

    file1 push

---
+++ Documents/Visual Studio 2015/Projects/ConsoleApplication3/ConsoleApplication3/RectangleArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int x = Area(2,3);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        //calculate the area of a rectangle
+        static int Area (int x, int y)
+        {
+            return (x * y);
+        }
+
+    }
+}
