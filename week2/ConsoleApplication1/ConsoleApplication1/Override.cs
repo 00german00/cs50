9816879aef751cde33291f7cbe9ae3c1f3d3fff8
German Fondevila <germanfondevila@gmail.com>
2016-10-10 20:42:23 -0400

    new class exercise -> override

---
+++ week2/ConsoleApplication1/ConsoleApplication1/Override.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.name = "John Matte";
+            s.age = 66;
+            s.hometown = "Charleston, SC";
+
+            Student s2 = new Student();
+            s2.name = "Matt Damon";
+            s2.age = 12;
+            s2.hometown = "Miami, FL";
+
+            Student s3 = new Student();
+            s3.name = "John Matte";
+            s3.age = 66;
+            s3.hometown = "Charleston, SC";
+
+            Console.WriteLine(s.ToString());
+            Console.WriteLine(s2.ToString());
+            Console.WriteLine(s3.ToString());
+            Console.WriteLine(s2.Equals(s));
+            Console.WriteLine(s.Equals(s3));
+            Console.ReadLine();           
+        }
+    }
+
+    class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string hometown { get; set; }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            result += "Name: " + name;
+            result += "\nAge: " + age;
+            result += "\nHometown: " + hometown;
+            result += "\n";
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Student compare = obj as Student;
+            if (compare != null)
+            {
+                if (compare.name == name && compare.age == age && compare.hometown == hometown)
+                {
+                    return true;
+                }
+                //return false;
+            }
+            return false;
+        }
+
+    }   
+
+}
---
+++ week2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie m = new Movie();
+            m.Title = "Gone in 60s";
+            m.Genre = "Action";
+
+            Movie m2 = new Movie();
+            m2.Title = "Gone in 30s";
+            m2.Genre = "Romance";
+
+            Movie m3 = new Movie();
+            m3.Title = "Gone in 60s";
+            m3.Genre = "Action";
+
+            Console.WriteLine(m2 == m);
+            Console.WriteLine(m2.Equals(m2));
+            Console.WriteLine(m2.Equals(m3));
+            Console.ReadLine();           
+        }
+    }
+
+
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            result += "Title:" + Title;
+            result += "\nGenre: " + Genre;
+
+            return result;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            // left value of as is what we want to cast and right is what we want to cast it to, and it returns a bool
+            Movie second = obj as Movie;
+            if (second != null)
+            {
+                if (second.Title == Title && second.Genre == Genre)
+                    {
+                        return true;
+                    }                
+            }
+            return false;
+        }
+    }
+
+
+    class Shape
+    {
+        public string Type { get; set; }
+               
+    }
+
+    class Circle: Shape
+    {
+
+    }
+}
